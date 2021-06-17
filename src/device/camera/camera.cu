010f5eb67742091600a5fb033954b56f5cdc669d
t4rf9 <t4rf9@163.com>
2021-06-17 17:31:16 +0800

    support specifying up of a camera which is not perpendicular to direction

--- src/device/camera/camera.cu
+++ src/device/camera/camera.cu
@@ -2,9 +2,9 @@
 
 __device__ Camera::Camera(const Vector3f &pos, const Vector3f &direction,
                           const Vector3f &up, int imgW, int imgH)
-    : pos(pos), direction(direction.normalized()), up(up.normalized()), width(imgW),
-      height(imgH) {
-    this->horizontal = Vector3f::cross(this->direction, this->up);
+    : pos(pos), direction(direction.normalized()), width(imgW), height(imgH) {
+    this->horizontal = Vector3f::cross(direction, up).normalized();
+    this->up = Vector3f::cross(horizontal, this->direction);
 }
 
 __device__ Camera::~Camera() {}
